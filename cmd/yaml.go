17279e8fba84d8dcefb1ebe12b8607a29f26f9da
Gary Bowers <gary@bowers1.com>
2021-01-29 10:44:40 +0000

    Update readme

--- cmd/yaml.go
+++ cmd/yaml.go
@@ -44,15 +44,6 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(yamlCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// yamlCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	yamlCmd.Flags().BoolP("input", "i", false, "Path to the input file to covert, must be in Openshift format")
 	yamlCmd.Flags().BoolP("output", "o", false, "Path to the output file for the results on the conversion")
 }
