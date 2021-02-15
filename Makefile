5e984b8caa26b4be1d412bf1b37b74fc86bcceec
Gary Bowers <gary@bowers1.com>
2021-02-15 09:01:45 +0000

    Garyb/templates (#2)
    * Adding generator functionality for helm
    
    * Remove unsupported objects
    
    * Remove testing output
    
    * Add ignore for testing output
    
    * Adding generator functionality for helm
    Remove unsupported objects
    Remove testing output
    Add ignore for testing output
    
    * Creating helm chart generation
    
    * Adding helm install to test

--- Makefile
+++ Makefile
@@ -2,11 +2,21 @@
 
 all: test
 
-test: clean
+test: clean templateTest lint apply
+
+lint:
+	helm lint ./out
+
+templateTest:
 	go run . template -i ./_test/os-nginx-template.yaml -o ./out -k helm
 
-clean: test
+clean: 
+	kubectl delete ns test
+	kubectl create ns test
 	rm -rf out
 
 build:
 	env GOOS=linux go build -v shifter
+
+apply:
+	helm install test ./out	
--- generator/helm.go
+++ generator/helm.go
@@ -3,7 +3,10 @@ package generator
 import (
 	"fmt"
 	"gopkg.in/yaml.v2"
+	"log"
 	"os"
+	"reflect"
+	"strconv"
 )
 
 type Chart struct {
@@ -13,6 +16,25 @@ type Chart struct {
 	KubeVersion string `yaml:"kubeVersion"`
 	Description string `yaml:"description"`
 	Type        string `yaml:"type"`
+	Icon        string `yaml:"icon"`
+}
+
+type kube struct {
+	Parameters []struct {
+		Name        string `yaml:"name"`
+		Description string `yaml:"description"`
+		Required    bool   `yaml:"required"`
+		Value       string `yaml:"value"`
+	}
+	Objects []object
+}
+
+type object struct {
+	ApiVersion string                 `yaml:"apiVersion"`
+	Kind       string                 `yaml:"kind"`
+	Metadata   map[string]interface{} //metadata has a unkown structure so we use a generic interface
+	Spec       map[string]interface{} `yaml:"spec,omitempty"` //specs are dependent on the kind so we use a generic interface
+	Data       map[string]interface{} `yaml:"data,omitempty"` //specs are dependent on the kind so we use a generic interface
 }
 
 // This needs to be broken out into a module!
@@ -34,7 +56,42 @@ func createFolderStruct(path string) {
 	}
 }
 
-func genChart() {
+func walk(v interface{}) {
+	fmt.Println(v)
+
+	val := reflect.ValueOf(v)
+	fmt.Println(val)
+	fmt.Println(val.Kind())
+
+	//	if val.Kind() == reflect.Map {
+	//		for _, e := range val.MapKeys() {
+	//			fmt.Println(e)
+	//			walk(e)
+	//		}
+	//	}
+
+	switch val.Kind() {
+	case reflect.Map:
+		for _, e := range val.MapKeys() {
+			fmt.Println(e)
+			walk(e)
+		}
+	default:
+		fmt.Println(val)
+	}
+}
+
+func mod(o object) {
+	for k, v := range o.Spec {
+		fmt.Println(k, v)
+		walk(v)
+		//matched, err := regexp.Match(`\${([^}]+)}`, []byte(v))
+		//fmt.Println(matched, err)
+		fmt.Println("\n")
+	}
+}
+
+func genChart(path string) {
 	var c Chart
 	c.ApiVersion = "v2"
 	c.Name = "test"
@@ -42,33 +99,75 @@ func genChart() {
 
 	cg, err := yaml.Marshal(&c)
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
-
-	fmt.Println(string(cg))
-}
-
-func genTemplate() {
-
-}
-
-func genValues() {
-}
-
-func CreateChart(path string) {
-	createFolderStruct(path)
 
 	chartFile, err := os.Create(path + "/Chart.yaml")
 	if err != nil {
-		fmt.Println(err)
+		log.Fatal(err)
 	}
 
-	valuesFile, err := os.Create(path + "/Values.yaml")
+	if _, err := chartFile.Write(cg); err != nil {
+		log.Fatal(err)
+	}
+	if err := chartFile.Close(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func genTemplate(objects kube, path string) {
+	for x, y := range objects.Objects {
+		mod(y)
+		content, err := yaml.Marshal(y)
+		if err != nil {
+			log.Fatal(err)
+		}
+		no := strconv.Itoa(x)
+		file, err := os.Create(path + "/templates/" + no + "-" + y.Kind + ".yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		if _, err := file.Write(content); err != nil {
+			log.Fatal(err)
+		}
+		if err := file.Close(); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func genValues(parameters kube, path string) {
+	m := make(map[interface{}]interface{})
+
+	for _, y := range parameters.Parameters {
+		m[y.Name] = y.Value
+	}
+
+	content, err := yaml.Marshal(m)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	file, err := os.Create(path + "/values.yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if _, err := file.Write(content); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func Generate(path string, input []byte) {
+	createFolderStruct(path)
+
+	var data kube
+	err := yaml.Unmarshal(input, &data)
 	if err != nil {
 		fmt.Println(err)
 	}
 
-	fmt.Println(chartFile)
-	fmt.Println(valuesFile)
-	genChart()
+	genTemplate(data, path)
+	genValues(data, path)
+	genChart(path)
 }
