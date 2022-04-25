37553b81a32b9ff104338029701261e0c11542ee
Tim Hiatt <timhiatt@google.com>
2022-04-25 07:18:39 +0000

    Constructing Mono Repo Structure

---
+++ shifter-ui/src/components/shifter/v1/status/healthz.vue
@@ -0,0 +1,39 @@
+<script setup>
+import {useShifterV1StatusHealthz} from '../../../../stores/shifter/v1/status/healthz'
+import WelcomeItem from '../../../WelcomeItem.vue';
+import DocumentationIcon from "../../../icons/IconDocumentation.vue";
+</script>
+
+<template>
+  <WelcomeItem>
+    <template #icon>
+      <!--<DocumentationIcon />-->
+    </template>
+    <template #heading>Shifter Server Status</template>
+
+   {{data.message}}
+  </WelcomeItem>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  computed: {
+    ...mapState(useShifterV1StatusHealthz, {data: 'results'})
+  },
+
+  methods: {
+    ...mapActions(useShifterV1StatusHealthz, ['fetchHealthz']),
+
+    addToCart() {
+      // we'll populate this later
+    }
+  },
+
+  created() {
+    // when the template is created, we call this action
+    this.fetchHealthz();
+  }
+};
+</script>
---
+++ shifter-ui/src/stores/shifter/v1/status/healthz.js
@@ -0,0 +1,58 @@
+import { shifterConfig } from "@/main"
+import axios from "axios"
+import { defineStore } from 'pinia'
+
+// API Endpoint Configuration
+const config = {
+    method: 'get',
+    url: (shifterConfig.API_BASE_URL+'/status/healthz'),
+    headers: {
+    
+    },
+    data : null
+};
+
+export const useShifterV1StatusHealthz = defineStore('shifter-v1-status-healthz', {
+  state: () => {
+    return {
+      data: {
+        message: "Running Connection Tests."
+      },
+      fetching: false
+    }
+  },
+
+  getters: {
+    results(state) {
+      return state.data;
+    },
+
+    isFetching(state) {
+      return state.fetching;
+    }
+  },
+
+  actions: {
+    async fetchHealthz() {
+      this.fetching = true;
+      try{
+        const response = await axios(config);
+      try {
+        const result = await response.data
+        this.data = result;
+        console.log(this.data)
+      } catch (err) {
+        this.data = [];
+        console.error('Error loading Shifter Server, Status, Healthz API:', err);
+        return err;
+      }
+      }
+      catch (err) {
+        this.data = {
+          message: "Shifter Server Unreachable, Timeout"
+        }
+      }
+      this.fetching = false;
+    }
+  }
+})
