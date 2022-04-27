d351d515ed4b57acb1d222d85b8db1a11049fe08
Tim Hiatt <timbohiatt@gmail.com>
2022-04-27 15:15:19 +0200

    Updating the docker_entrypoint.sh for Shifter-UI

--- shifter-ui/docker_entrypoint.sh
+++ shifter-ui/docker_entrypoint.sh
@@ -1,27 +1,43 @@
 #!/bin/bash
-
-echo "--------------------"
-echo "--------------------"
-echo "   I AM RUNNING "
-echo "--------------------"
-echo "--------------------"
-
-# Remove Current Assets Folder Including All Previous Modifications
-rm -rf code/assets/
-# Utilize Backup Copy.
-cp -r code/assets_originals/ code/assets/
-
-########################################
-# Replace all References to Environment Variables  
-# within JS Files.
-########################################
+export FILE="/code/shifter.server.endpoint"
 export EXISTING_VARS=$(printenv | awk -F= '{print $1}' | sed 's/^/\$/g' | paste -sd,); 
-for file in $JSFOLDER;
-do
-  cat $file | envsubst $EXISTING_VARS | tee $file
-done
 
+if [ -f "$FILE" ]; then
+  PREVIOUS_ENDPOINT=$(cat ${FILE})
+  if [ "$PREVIOUS_ENDPOINT" = "$SHIFTER_SERVER_ENDPOINT" ]; then
+    echo "Note: shifter.server.endpoint is Unchanged."
+  else
+    echo "Note: shifter.server.endpoint has Changed."
+    echo "---> Previous Endpoint: ${PREVIOUS_ENDPOINT}"
+    echo "---> Updated  Endpoint: ${SHIFTER_SERVER_ENDPOINT}"
+    rm -rf /code/assets
+    cp -r /code/assets_original /code/assets
+    rm -rf $FILE
+  fi
+else
+  echo "Note: This is an Initial Run."
+fi
 
+if [ ! -e "$FILE" ]; then
+    ########################################
+    # Replace all References to Environment Variables  
+    # within JS Files.
+    ########################################
+    for jsFile in $JSFOLDER;
+    do
+      
+      cat $jsFile | envsubst $EXISTING_VARS | tee $jsFile
+
+    done
+    echo $SHIFTER_SERVER_ENDPOINT > $FILE
+    echo "------> Initial Boot."
+    cat $FILE
+    echo $SHIFTER_SERVER_ENDPOINT
+else 
+    echo "------> Secondry Boot."
+    cat $FILE 
+    echo $SHIFTER_SERVER_ENDPOINT
+fi
 
 ########################################
 # Start NGINX Server
