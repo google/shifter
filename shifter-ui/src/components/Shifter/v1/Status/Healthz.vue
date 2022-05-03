a550becd158b156ec4cb03bd700484c7ab818c8b
Tim Hiatt <timhiatt@google.com>
2022-05-03 08:36:22 +0000

    Added NavBar and Routing as Well as some Simple Form Concepts

---
+++ shifter-ui/src/components/Shifter/v1/Status/Healthz.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useShifterV1StatusHealthz } from '../../../../stores/shifter/v1/status/healthz'
+</script>
+
+<template>
+  <ui>
+    <li>Shifter Server Status: {{data.message}}</li>
+  </ui>
+</template>
+
+<script>
+import { mapState, mapActions } from 'pinia'
+
+export default {
+  computed: {
+    ...mapState(useShifterV1StatusHealthz, {data: 'results'})
+  },
+
+  methods: {
+    ...mapActions(useShifterV1StatusHealthz, ['fetchHealthz']),
+  },
+
+  created() {
+    // when the template is created, we call this action
+    this.fetchHealthz();
+  }
+};
+</script>
--- shifter-ui/tailwind.config.js
+++ shifter-ui/tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'shifter-black': '#181818',
+      'shifter-black-soft': '#222222',
+      'shifter-black-mute': '#282828',
+
+      'shifter-white': '#ffffff',
+      'shifter-white-soft': '#f8f8f8',
+      'shifter-white-mute': '#f2f2f2',
+
+      'shifter-red': '#db212d',
+      'shifter-red-soft': '#bd1c27',
+      'shifter-red-muted': '#a61e27',
+
+      'dummy-green': '#4cbd46',
+      'dummy-blue': '#6995cf'
+    },
+    
     extend: {},
   },
   plugins: [],
