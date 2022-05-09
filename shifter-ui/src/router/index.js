bbc21c4e4d78d9fb5c0283f6c78ea19c64a43f50
Tim Hiatt <timbohiatt@gmail.com>
2022-05-09 09:54:27 +0100

    basics of convert page and form

--- shifter-ui/src/router/index.js
+++ shifter-ui/src/router/index.js
@@ -14,9 +14,17 @@ const router = createRouter({
       path: "/about",
       name: "about",
       // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
+      // this generates a separate chunk (AboutView.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/AboutView.vue"),
+    },
+    {
+      path: "/convert",
+      name: "convert",
+      // route level code-splitting
+      // this generates a separate chunk (Convert.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import("../views/Convert.vue"),
     },
     {
       path: "/configure",
