7b6ad33ee76f66fb85e1c82027caff85d7e7b0b5
Tim Hiatt <timbohiatt@gmail.com>
2022-06-08 19:12:36 +0200

    before adding material design icon pack

--- shifter-ui/src/components/list-convert-namespace-objects.vue
+++ shifter-ui/src/components/list-convert-namespace-objects.vue
@@ -4,7 +4,7 @@ import ListConvertObjectsItem from "./list-convert-namespace-objects-item.vue";
 </script>
 <template>
   <div class="container flex-row mx-auto justify-center mx-4">
-    <p class="text-lg font-bold">OpenShift Namespaces</p>
+    <p class="text-lg font-bold">OpenShift Namespaces ({{ itemCount }})</p>
     <ListConvertObjectsItem
       class="flex-row mx-auto justify-left border border-shifter-black m-3"
       v-for="namespace in all"
@@ -28,6 +28,12 @@ export default {
 
   computed: {
     ...mapState(useOSProjects, { all: "all" }),
+    itemCount() {
+      if (this.all !== undefined && this.all.length >= 0) {
+        return this.all.length;
+      }
+      return 0;
+    },
   },
 };
 </script>
