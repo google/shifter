2e956a4be698e24b925d728ff6a3a527088b53ac
Tim Hiatt <timbohiatt@gmail.com>
2022-06-13 14:36:38 +0100

    Local storage for Cluster Configurations

--- shifter-ui/src/stores/configurations/clusters.js
+++ shifter-ui/src/stores/configurations/clusters.js
@@ -9,64 +9,6 @@ export const useConfigurationsClusters = defineStore(
     state: () => {
       return {
         clusters: useStorage("shifter-config-clusters", []),
-        /*clusters: [
-          {
-            id: 0,
-            enabled: true,
-            shifter: {
-              clusterConfig: {
-                connectionName: "OKD Shifter DEV Server v3.11",
-                username: "",
-                password: "",
-                baseUrl: "https://console.okd.shifter.cloud:8443",
-                bearerToken:
-                  "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InJvYm90LXRva2VuLThod3dtIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6InJvYm90Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiNGIyZjdmZDItYmZmOS0xMWVjLWI5Y2MtNDIwMTBhMDAwMDAzIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6cm9ib3QifQ.BshucYnBWMlBPtcGWBk8EbCGWGTjXYWoExrspYKvCnLrP8Iqs4p-SA-3RCvZ8fe0JyxwE-1GIDIgaxH9270303BGXPcdEJFUNW06j_ZPv5EZYn-C8UQxCmRpfZBe9QdtOEre6YSmcL-GrfE4fdaHAkYN1Yv5Fr6k_JPt3XGRHD2Hy0By14eFG7oW3b2rj15_lYWW-vfQaYSbcG8HViPF62lqQL0KSZwySOC82jpMdKUCdtuuBC01Gzd9NfTGTeBo3VSQ6mTTOchL1T9GX6oD-wu4J9NOmbPYZPln0Q-_8h6H488Mc4xTfzLdkb136-GJAwT7GgksYm3UMx_sXkj36A",
-              },
-            },
-          },
-          {
-            id: 1,
-            enabled: false,
-            shifter: {
-              clusterConfig: {
-                connectionName: "OKD Shifter TST Server v3.11",
-                username: "",
-                password: "",
-                baseUrl: "https://console.okd.shifter.cloud:8443",
-                bearerToken:
-                  "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InJvYm90LXRva2VuLThod3dtIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6InJvYm90Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiNGIyZjdmZDItYmZmOS0xMWVjLWI5Y2MtNDIwMTBhMDAwMDAzIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6cm9ib3QifQ.BshucYnBWMlBPtcGWBk8EbCGWGTjXYWoExrspYKvCnLrP8Iqs4p-SA-3RCvZ8fe0JyxwE-1GIDIgaxH9270303BGXPcdEJFUNW06j_ZPv5EZYn-C8UQxCmRpfZBe9QdtOEre6YSmcL-GrfE4fdaHAkYN1Yv5Fr6k_JPt3XGRHD2Hy0By14eFG7oW3b2rj15_lYWW-vfQaYSbcG8HViPF62lqQL0KSZwySOC82jpMdKUCdtuuBC01Gzd9NfTGTeBo3VSQ6mTTOchL1T9GX6oD-wu4J9NOmbPYZPln0Q-_8h6H488Mc4xTfzLdkb136-GJAwT7GgksYm3UMx_sXkj36A",
-              },
-            },
-          },
-          {
-            id: 2,
-            enabled: true,
-            shifter: {
-              clusterConfig: {
-                connectionName: "OKD Shifter PRD Server v3.11",
-                username: "",
-                password: "",
-                baseUrl: "https://console.okd.shifter.cloud:8443",
-                bearerToken:
-                  "eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJkZWZhdWx0Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZWNyZXQubmFtZSI6InJvYm90LXRva2VuLThod3dtIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQubmFtZSI6InJvYm90Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiNGIyZjdmZDItYmZmOS0xMWVjLWI5Y2MtNDIwMTBhMDAwMDAzIiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50OmRlZmF1bHQ6cm9ib3QifQ.BshucYnBWMlBPtcGWBk8EbCGWGTjXYWoExrspYKvCnLrP8Iqs4p-SA-3RCvZ8fe0JyxwE-1GIDIgaxH9270303BGXPcdEJFUNW06j_ZPv5EZYn-C8UQxCmRpfZBe9QdtOEre6YSmcL-GrfE4fdaHAkYN1Yv5Fr6k_JPt3XGRHD2Hy0By14eFG7oW3b2rj15_lYWW-vfQaYSbcG8HViPF62lqQL0KSZwySOC82jpMdKUCdtuuBC01Gzd9NfTGTeBo3VSQ6mTTOchL1T9GX6oD-wu4J9NOmbPYZPln0Q-_8h6H488Mc4xTfzLdkb136-GJAwT7GgksYm3UMx_sXkj36A",
-              },
-            },
-          },
-          {
-            id: 3,
-            enabled: true,
-            shifter: {
-              clusterConfig: {
-                connectionName:
-                  "OKD Shifter PRD Server v3.11 Username & Password",
-                username: "shifter",
-                password: "P4@FpHbMn&YqFEfn",
-                baseUrl: "https://console.okd.shifter.cloud:8443",
-                bearerToken: "",
-              },
-            },
-          },
-        ],*/
         fetching: false,
       };
     },
