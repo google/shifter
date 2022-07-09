cc0f25b88a75cc4962e9631ea8f5b0edf4232ee3
Tim Hiatt <timbohiatt@gmail.com>
2022-07-10 01:58:24 +0200

    Cleaning up NGINX content and images

--- build-deploy-local.sh
+++ build-deploy-local.sh
@@ -1,11 +1,9 @@
 # Removing Existing Local Development Containers
-docker container rm -f shifter_nginx
 docker container rm -f shifter_ui_development 
 docker container rm -f shifter_server_development 
 docker container prune -f
 
 # Removing Existing Local Development Images
-docker image rm --force local.images.shifter.cloud/shifter-nginx:latest
 docker image rm --force local.images.shifter.cloud/shifter-ui:latest
 docker image rm --force local.images.shifter.cloud/shifter:latest
 docker image prune -f
@@ -16,7 +14,6 @@ docker network create shifter-network
 
 
 # Building Local Development Images for Shifter UI and Shifter Server
-docker build  --no-cache -t local.images.shifter.cloud/shifter-nginx:latest -f "./shifter-ui/nginx/Dockerfile" ./shifter-ui/nginx/
 docker build  --no-cache -t local.images.shifter.cloud/shifter-ui:latest -f "./shifter-ui/Dockerfile" ./shifter-ui/
 docker build  --no-cache -t local.images.shifter.cloud/shifter:latest -f "./shifter/Dockerfile" ./shifter/
 
