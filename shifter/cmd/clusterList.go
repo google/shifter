5610a125d5fa5eb7ff60f45cea0fad797a28a849
Tim Hiatt <timhiatt@google.com>
2022-08-16 10:23:47 +0000

    Updating some docs

--- shifter/cmd/clusterList.go
+++ shifter/cmd/clusterList.go
@@ -26,12 +26,7 @@ import (
 var clusterListCmd = &cobra.Command{
 	Use:   "list",
 	Short: "Lists all resources supported by shifter in the target Openshift cluster.",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Long:  "Lists all resources supported by shifter in the target Openshift cluster.",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println(`
    _____ __    _ ______
