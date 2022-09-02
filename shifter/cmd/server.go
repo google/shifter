5cd345148add3391f4fddb1f317374d6be64692c
Gary Bowers <garybowers@google.com>
2022-09-02 09:01:57 +0100

    Updates

--- shifter/cmd/server.go
+++ shifter/cmd/server.go
@@ -30,12 +30,12 @@ var serverCmd = &cobra.Command{
 	Use:   "server",
 	Short: "Convert Openshift Resources to Kubernetes native formats via Shifter API",
 	Long: `
-   _____ __    _ ______            
+   _____ __    _ ______
   / ___// /_  (_) __/ /____  _____       ___   ____ ____ ______  ___    ___   ____
   \__ \/ __ \/ / /_/ __/ _ \/ ___/      / _ \ / __// __//_  __/ / _ |  / _ \ /  _/
  ___/ / / / / / __/ /_/  __/ /         / , _// _/ _\ \   / /   / __ | / ___/_/ /
-/____/_/ /_/_/_/  \__/\___/_/         /_/|_|/___//___/  /_/   /_/ |_|/_/   /___/ 
-                                 
+/____/_/ /_/_/_/  \__/\___/_/         /_/|_|/___//___/  /_/   /_/ |_|/_/   /___/
+
 Convert OpenShift resources to kubernetes native formats
 
 Usage: shifter server
@@ -44,12 +44,12 @@ Usage: shifter server
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("\033[31m" + `
 
-   _____ __    _ ______            
+   _____ __    _ ______
   / ___// /_  (_) __/ /____  _____       ___   ____ ____ ______  ___    ___   ____
   \__ \/ __ \/ / /_/ __/ _ \/ ___/      / _ \ / __// __//_  __/ / _ |  / _ \ /  _/
  ___/ / / / / / __/ /_/  __/ /         / , _// _/ _\ \   / /   / __ | / ___/_/ /
-/____/_/ /_/_/_/  \__/\___/_/         /_/|_|/___//___/  /_/   /_/ |_|/_/   /___/ 
-                               
+/____/_/ /_/_/_/  \__/\___/_/         /_/|_|/___//___/  /_/   /_/ |_|/_/   /___/
+
 -------------------------------------------------------------------------------------
 			` + "\033[0m")
 
@@ -71,7 +71,7 @@ Usage: shifter server
 func init() {
 	// TODO - Revisit the Flags and Required Flags, Fix descriptions add options, Add valdations.
 	rootCmd.AddCommand(serverCmd)
-	serverCmd.Flags().StringVarP(&serverPort, "port", "p", "8080", "Server Port: Default 8080")
+	serverCmd.Flags().StringVarP(&serverPort, "port", "p", "8082", "Server Port: Default 8082")
 	serverCmd.Flags().StringVarP(&serverAddress, "host-address", "a", "0.0.0.0", "Host Address: Default 0.0.0.0")
 	serverCmd.Flags().StringVarP(&sourcePath, "source-path", "f", "", "Relative Local Path (./data/source) or Google Cloud Storage Bucket Path (gs://XXXXXXX/source/) for Source Files to be Written")
 	serverCmd.Flags().StringVarP(&outputPath, "output-path", "o", "", "Relative Local Path (./data/output) or Google Cloud Storage Bucket Path (gs://XXXXXXX/output/) for Converted Files to be Written")
