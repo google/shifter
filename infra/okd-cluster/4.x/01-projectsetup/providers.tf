6efa3c61558e5fb118e008e23e1ca4a4679c11d4
parasmamgain <mamgain.paras@outlook.com>
2022-11-11 12:38:31 +0000

    resolve conflicts

--- infra/okd-cluster/4.x/01-projectsetup/providers.tf
+++ infra/okd-cluster/4.x/01-projectsetup/providers.tf
@@ -1,14 +1,15 @@
 
+#Update the service accounts
 provider "google" {
-  impersonate_service_account = "okd-sa@pm-singleproject-20.iam.gserviceaccount.com"
+  impersonate_service_account = "okd-sa@PROJECT-ID.iam.gserviceaccount.com"
 }
 provider "google-beta" {
-  impersonate_service_account = "okd-sa@pm-singleproject-20.iam.gserviceaccount.com"
+  impersonate_service_account = "okd-sa@PROJECT-ID.iam.gserviceaccount.com"
 }
 terraform {
   backend "gcs" {
     bucket                      = "shifter-tfstate"
     prefix                      = "shifter/4.x"
-    impersonate_service_account = "okd-sa@pm-singleproject-20.iam.gserviceaccount.com"
+    impersonate_service_account = "okd-sa@PROJECT-ID.iam.gserviceaccount.com"
   }
 }
