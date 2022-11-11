b7ddb1e953bc05236a380c255b6f0e7dcca3484b
mamgain.paras@outlook.com <parasmamgain@google.com>
2022-11-11 11:06:05 +0000

    remove unused example

--- infra/okd-cluster/examples/rail-ex/app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    render :file => 'public/index.html'
-  end
-end
--- infra/okd-cluster/examples/rail-ex/migrate-database.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-
-echo "Executing bundle exec 'rake db:migrate' ..."
-RAILS_ENV=production bundle exec rake db:migrate
