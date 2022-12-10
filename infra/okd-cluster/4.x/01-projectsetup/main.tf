f147800ec21e3aa5331ff451f44be161e0a3b548
parasmamgain <mamgain.paras@outlook.com>
2022-12-10 05:51:15 +0000

    update trigger

--- infra/okd-cluster/4.x/01-projectsetup/main.tf
+++ infra/okd-cluster/4.x/01-projectsetup/main.tf
@@ -255,8 +255,8 @@ resource "google_cloudbuild_trigger" "createresource-trigger" {
         <<-EOT
             echo "******************************************"
             terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke init &&
-            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke plan -var=$_PROJECT_NAME &&
-            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke apply -var=$_PROJECT_NAME -auto-approve
+            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke plan -var="project_id=$_PROJECT_NAME" &&
+            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke apply -var="project_id=$_PROJECT_NAME" -auto-approve
         EOT
       ]
     }
@@ -313,8 +313,8 @@ resource "google_cloudbuild_trigger" "deletecluster-trigger" {
         <<-EOT
             echo "******************************************"
             terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke init &&
-            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke plan -var=$_PROJECT_NAME &&
-            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke destroy -var=$_PROJECT_NAME -auto-approve
+            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke plan -var="project_id=$_PROJECT_NAME" &&
+            terraform -chdir=/workspace/infra/okd-cluster/4.x/03-gke destroy -var="project_id=$_PROJECT_NAME" -auto-approve
         EOT
       ]
     }
