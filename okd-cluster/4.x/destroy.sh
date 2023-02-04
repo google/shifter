4b8051b4f3ac0fb37aea331fe3fa883135fcd679
akshanshck <123233309+akshanshck@users.noreply.github.com>
2023-02-04 22:20:29 +0530

    Update destroy.sh

--- okd-cluster/4.x/destroy.sh
+++ okd-cluster/4.x/destroy.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+echo "[!] Processing...."
 ###################### Mandatory Vairables ##########################
 PROJECT_ID=""     # e.g. "pm-okd-11"
 CLUSTER_NAME=""   # e.g."okd-41"
